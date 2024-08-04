8b5ee304cb66e87a072d6133c327f5ec11cfa364
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-03 21:46:42 -0300

    Created a builder class for Borrower

---
+++ v1/src/main/java/com/api/v1/borrower/builder/BorrowerBuilder.java
@@ -0,0 +1,69 @@
+package com.api.v1.borrower.builder;
+
+import com.api.v1.borrower.domain.Borrower;
+
+public class BorrowerBuilder {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String ssn;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String gender;
+
+    public BorrowerBuilder withFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public BorrowerBuilder withMiddleName(String middleName) {
+        this.middleName = middleName;
+        return this;
+    }
+
+    public BorrowerBuilder withLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public BorrowerBuilder withSsn(String ssn) {
+        this.ssn = ssn;
+        return this;
+    }
+
+    public BorrowerBuilder withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public BorrowerBuilder withAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public BorrowerBuilder withPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public BorrowerBuilder withGender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public Borrower build() {
+        return new Borrower(
+            this.firstName,
+            this.middleName,
+            this.lastName,
+            this.ssn,
+            this.email,
+            this.address,
+            this.phoneNumber, 
+            this.gender
+        );
+    }    
+
+}
