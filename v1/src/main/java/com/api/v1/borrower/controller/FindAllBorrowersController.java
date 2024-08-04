276f067ada4d4481fc15b25bc7b01a85a60eff90
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-04 15:53:16 -0300

    Fixed FindAllBorrowersController

--- v1/src/main/java/com/api/v1/borrower/controller/FindAllBorrowersController.java
+++ v1/src/main/java/com/api/v1/borrower/controller/FindAllBorrowersController.java
@@ -1,6 +1,16 @@
 package com.api.v1.borrower.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.v1.borrower.dtos.BorrowerResponse;
 import com.api.v1.borrower.services.FindAllBorrowersService;
+
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("api/v1/borrowers")
@@ -10,7 +20,7 @@ class FindAllBorrowersController {
     private FindAllBorrowersService service;
 
     @GetMapping
-    @ResponseStatus(status = HttpStatus.OK)
+    @ResponseStatus(value = HttpStatus.OK)
     public Flux<BorrowerResponse> findAll() {
         return service.findAll();
     }
