0233f350fb86b0c78d4423486e42b259b562f264
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-03 21:47:12 -0300

    Created two dtos for Borrower

---
+++ v1/src/main/java/com/api/v1/borrower/dtos/BorrowerResponse.java
@@ -0,0 +1,16 @@
+package com.api.v1.borrower.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record BorrowerResponse(
+        @NotBlank String fullName,
+        @NotNull @Size(min=9, max=9) String ssn,
+        @NotNull @Email String email,
+        @NotBlank String address,
+        @NotNull @Size(min=10, max=10) String phoneNumber,
+        @NotNull @Size(min=1) String gender
+) {
+}
