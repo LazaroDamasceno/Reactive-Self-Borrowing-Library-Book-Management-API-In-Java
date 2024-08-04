73159b033a6b9a7bcdd7dd955d44e96c2cec3430
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-04 15:48:20 -0300

    Fixed FindAllBorrowersService and FindAllBorrowersServiceImpl

--- v1/src/main/java/com/api/v1/borrower/services/FindAllBorrowersService.java
+++ v1/src/main/java/com/api/v1/borrower/services/FindAllBorrowersService.java
@@ -1,5 +1,9 @@
 package com.api.v1.borrower.services;
 
+import com.api.v1.borrower.dtos.BorrowerResponse;
+
+import reactor.core.publisher.Flux;
+
 public interface FindAllBorrowersService {
 
     Flux<BorrowerResponse> findAll();
--- v1/src/main/java/com/api/v1/borrower/services/FindAllBorrowersServiceImpl.java
+++ v1/src/main/java/com/api/v1/borrower/services/FindAllBorrowersServiceImpl.java
@@ -1,7 +1,14 @@
 package com.api.v1.borrower.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.v1.borrower.domain.Borrower;
 import com.api.v1.borrower.domain.BorrowerRepository;
+import com.api.v1.borrower.dtos.BorrowerResponse;
 import com.api.v1.borrower.mapper.BorrowerMapper;
+
+import reactor.core.publisher.Flux;
 
 @Service
 class FindAllBorrowersServiceImpl implements FindAllBorrowersService {
@@ -9,6 +16,7 @@ class FindAllBorrowersServiceImpl implements FindAllBorrowersService {
     @Autowired
     private BorrowerRepository repository;
 
+    @Override
     public Flux<BorrowerResponse> findAll() {
         Flux<Borrower> borrowers = repository.findAll();
         return BorrowerMapper.mapFromFlux(borrowers);
