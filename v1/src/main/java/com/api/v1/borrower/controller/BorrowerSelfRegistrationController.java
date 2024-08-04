946601f51a1a844869b72a6c635f432a1d5b8342
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-03 23:36:31 -0300

    Changed BorrowerSelfRegistrationController

--- v1/src/main/java/com/api/v1/borrower/controller/BorrowerSelfRegistrationController.java
+++ v1/src/main/java/com/api/v1/borrower/controller/BorrowerSelfRegistrationController.java
@@ -2,6 +2,7 @@ package com.api.v1.borrower.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.v1.borrower.dtos.BorrowerRequest;
+import com.api.v1.borrower.dtos.BorrowerResponse;
 import com.api.v1.borrower.services.BorrowerSelfRegistrationService;
 
 import jakarta.validation.Valid;
@@ -23,9 +25,9 @@ class BorrowerSelfRegistrationController {
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
-    public Mono<Void> selfRegister(@Valid @RequestBody BorrowerRequest request) {
-        service.sefRegister(request);   
-        return Mono.empty();
+    public ResponseEntity<Mono<BorrowerResponse>> selfRegister(@Valid @RequestBody BorrowerRequest request) {
+        Mono<BorrowerResponse> response = service.sefRegister(request);   
+        return ResponseEntity.status(201).body(response);
     }
 
 }
