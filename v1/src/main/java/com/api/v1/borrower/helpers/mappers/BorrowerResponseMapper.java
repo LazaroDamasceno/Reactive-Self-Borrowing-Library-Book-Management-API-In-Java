f1d8d445f039155782fedee513e884c1920afdb3
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-05 16:51:27 -0300

    Added the annotation Component to the BorrowerResponseMapper

--- v1/src/main/java/com/api/v1/borrower/helpers/mappers/BorrowerResponseMapper.java
+++ v1/src/main/java/com/api/v1/borrower/helpers/mappers/BorrowerResponseMapper.java
@@ -2,7 +2,9 @@ package com.api.v1.borrower.helpers.mappers;
 
 import com.api.v1.borrower.domain.Borrower;
 import com.api.v1.borrower.helpers.dtos.BorrowerResponse;
+import org.springframework.stereotype.Component;
 
+@Component
 final class BorrowerResponseMapper {
 
     public BorrowerResponse mapFromBorrower(Borrower borrower) {
