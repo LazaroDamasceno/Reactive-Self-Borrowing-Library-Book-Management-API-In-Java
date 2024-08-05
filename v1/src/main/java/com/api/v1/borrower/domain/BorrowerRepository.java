ad5eddc177291fa698b443e79d497f1e88973a22
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-04 22:08:26 -0300

    Updated BorrowerRepository

--- v1/src/main/java/com/api/v1/borrower/domain/BorrowerRepository.java
+++ v1/src/main/java/com/api/v1/borrower/domain/BorrowerRepository.java
@@ -2,8 +2,14 @@ package com.api.v1.borrower.domain;
 
 import java.util.UUID;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import reactor.core.publisher.Mono;
 
 public interface BorrowerRepository extends ReactiveCrudRepository<Borrower, UUID> {
     
+    @Query("{ 'ssn' : { $eq : ?0 } }")
+    Mono<Borrower> getBySsn(String ssn);
+
 }
