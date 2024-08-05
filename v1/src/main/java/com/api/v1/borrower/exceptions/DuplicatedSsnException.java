7f90da298b6d3d25bf5da28cadde8525e56c5405
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-05 01:42:30 -0300

    Deleted DuplicatedSsnException

--- v1/src/main/java/com/api/v1/borrower/exceptions/DuplicatedSsnException.java
+++
@@ -1,9 +0,0 @@
-package com.api.v1.borrower.exceptions;
-
-public class DuplicatedSsnException extends RuntimeException {
-
-    public DuplicatedSsnException() {
-        super("The input was already registered. Please, inout another one.");
-    }
-    
-}
