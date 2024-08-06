f8838cfa792af90e31f528929d4b112f05ccc5d2
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-06 17:11:09 -0300

    Removed thr attribute isbn from NewBookRequest

--- v1/src/main/java/com/api/v1/book/helpers/NewBookRequest.java
+++ v1/src/main/java/com/api/v1/book/helpers/NewBookRequest.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.Size;
 public record NewBookRequest(
         @NotBlank String title,
         String subtitle,
-        @NotNull @Size(min=13, max=13) String isbn,
         @NotBlank String author,
         @NotBlank String field,
         @NotBlank int numberOfPages,
