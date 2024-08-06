76521cb10d53016ab15e775776ec2d26ab2ac4e3
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-06 17:45:23 -0300

    Updated BorrowerBuilder

--- v1/src/main/java/com/api/v1/borrower/builder/BorrowerBuilder.java
+++ v1/src/main/java/com/api/v1/borrower/builder/BorrowerBuilder.java
@@ -3,9 +3,14 @@ package com.api.v1.borrower.builder;
 import com.api.v1.borrower.domain.Borrower;
 import org.springframework.stereotype.Component;
 
+import java.time.ZonedDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
 @Component
 public class BorrowerBuilder {
 
+    private final UUID id = UUID.randomUUID();
     private String firstName;
     private String middleName;
     private String lastName;
@@ -14,6 +19,7 @@ public class BorrowerBuilder {
     private String address;
     private String phoneNumber;
     private String gender;
+    private final String createdAt = ZonedDateTime.now().toString();
 
     public BorrowerBuilder withFirstName(String firstName) {
         this.firstName = firstName;
@@ -21,7 +27,7 @@ public class BorrowerBuilder {
     }
 
     public BorrowerBuilder withMiddleName(String middleName) {
-        this.middleName = middleName;
+        this.middleName = Objects.requireNonNull(middleName);
         return this;
     }
 
@@ -57,6 +63,7 @@ public class BorrowerBuilder {
 
     public Borrower build() {
         return new Borrower(
+            this.id,
             this.firstName,
             this.middleName,
             this.lastName,
@@ -64,7 +71,8 @@ public class BorrowerBuilder {
             this.email,
             this.address,
             this.phoneNumber, 
-            this.gender
+            this.gender,
+            this.createdAt
         );
     }    
 
