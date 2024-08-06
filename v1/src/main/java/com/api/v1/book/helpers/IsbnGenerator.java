f803a099016f3d2a3231e53158dc9c8ff881d91b
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-06 17:12:20 -0300

    Turned IsbnGenerator final and turned its method generateIsbn static.

--- v1/src/main/java/com/api/v1/book/helpers/IsbnGenerator.java
+++ v1/src/main/java/com/api/v1/book/helpers/IsbnGenerator.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
 
-@Component
-public class IsbnGenerator {
+public final class IsbnGenerator {
 
-    public String generateIsbn() {
+    public static String generateIsbn() {
         long min = 1000000000000L;
         long max = 9999999999999L;
         long isbn = new SecureRandom().nextLong(min, max+1);
