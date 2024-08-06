5ecf1b2f46e44d8412ba8f2430b64e1edf6594cf
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-06 17:37:17 -0300

    Created a builder for Book

---
+++ v1/src/main/java/com/api/v1/book/builder/BookBuilder.java
@@ -0,0 +1,78 @@
+package com.api.v1.book.builder;
+
+import com.api.v1.book.domain.Book;
+import com.api.v1.book.helpers.IsbnGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Component
+public class BookBuilder {
+
+    @Autowired
+    private IsbnGenerator isbnGenerator;
+
+    private final UUID id = UUID.randomUUID();
+    private String title;
+    private String subtitle;
+    private final String isbn = isbnGenerator.generateIsbn();
+    private String author;
+    private String field;
+    private int numberOfPages;
+    private int version;
+    private String description;
+    private final String addedAt = ZonedDateTime.now().toString();
+
+    public BookBuilder withTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public BookBuilder withSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+        return this;
+    }
+
+    public BookBuilder withAuthor(String author) {
+        this.author = author;
+        return this;
+    }
+
+    public BookBuilder withField(String field) {
+        this.field = field;
+        return this;
+    }
+
+    public BookBuilder withNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+        return this;
+    }
+
+    public BookBuilder withVersion(int version) {
+        this.version = version;
+        return this;
+    }
+
+    public BookBuilder withDescription(String description) {
+        this.description = description;
+        return this;
+    }
+    
+    public Book build() {
+        return new Book(
+                this.id,
+                this.title,
+                this.subtitle,
+                this.isbn,
+                this.author,
+                this.field,
+                this.numberOfPages,
+                this.version,
+                this.description,
+                this.addedAt
+        );
+    }
+    
+}
