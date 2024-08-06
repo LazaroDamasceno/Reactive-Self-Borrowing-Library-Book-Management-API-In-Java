1493983840cab0e94b699ac89d1ce874dd63e0dd
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-06 17:00:50 -0300

    Removed unused imports from UpdatedBookRequest

--- v1/src/main/java/com/api/v1/book/helpers/UpdatedBookRequest.java
+++ v1/src/main/java/com/api/v1/book/helpers/UpdatedBookRequest.java
@@ -1,8 +1,6 @@
 package com.api.v1.book.helpers;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record UpdatedBookRequest(
         @NotBlank String title,
