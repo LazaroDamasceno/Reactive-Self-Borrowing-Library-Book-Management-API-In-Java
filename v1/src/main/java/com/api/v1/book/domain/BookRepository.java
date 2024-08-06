bad72dc4890247494a50419141fc36ca815886f4
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-06 00:28:10 -0300

    Fixed the repository of Book

---
+++ v1/src/main/java/com/api/v1/book/domain/BookRepository.java
@@ -0,0 +1,14 @@
+package com.api.v1.book.domain;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public interface BookRepository extends ReactiveCrudRepository<Book, UUID> {
+
+    @Query("{ isbn: { $eq: ?0  } }")
+    Mono<Book> getByIsbn(String isbn, int version);
+
+}
