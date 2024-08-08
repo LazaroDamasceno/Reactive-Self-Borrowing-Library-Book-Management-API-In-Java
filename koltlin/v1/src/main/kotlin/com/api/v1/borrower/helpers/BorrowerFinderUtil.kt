a76985e7356d3af4d09330fb9cf1947bc56011f7
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-08 02:59:44 -0300

    Changed BorrowerFinderUtil

--- koltlin/v1/src/main/kotlin/com/api/v1/borrower/helpers/BorrowerFinderUtil.kt
+++ koltlin/v1/src/main/kotlin/com/api/v1/borrower/helpers/BorrowerFinderUtil.kt
@@ -3,17 +3,20 @@ package com.api.v1.borrower.helpers
 import com.api.v1.borrower.domain.Borrower
 import com.api.v1.borrower.domain.BorrowerRepository
 import com.api.v1.borrower.exceptions.BorrowerNotFoundException
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 
 @Component
-class BorrowerFinderUtil(private val repository: BorrowerRepository) {
+class BorrowerFinderUtil {
+
+     @Autowired
+     private lateinit var repository: BorrowerRepository
 
      fun find(ssn: String): Mono<Borrower> {
          return repository
              .getBySsn(ssn)
              .switchIfEmpty(Mono.error(BorrowerNotFoundException()))
-             .cache()
      }
 
 }
