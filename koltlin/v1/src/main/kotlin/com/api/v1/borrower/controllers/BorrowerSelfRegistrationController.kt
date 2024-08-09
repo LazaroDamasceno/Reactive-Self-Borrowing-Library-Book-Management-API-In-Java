185abb3fa6a06513f7da794bd9d0017d943ac828
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-09 09:56:12 -0300

    Updated BorrowerSelfRegistrationServiceImpl.kt

--- koltlin/v1/src/main/kotlin/com/api/v1/borrower/controllers/BorrowerSelfRegistrationController.kt
+++ koltlin/v1/src/main/kotlin/com/api/v1/borrower/controllers/BorrowerSelfRegistrationController.kt
@@ -1,7 +1,7 @@
 package com.api.v1.borrower.controllers
 
-import com.api.v1.borrower.helpers.BorrowerResponseDto
-import com.api.v1.borrower.helpers.NewBorrowerRequestDto
+import com.api.v1.borrower.dtos.BorrowerResponseDto
+import com.api.v1.borrower.dtos.NewBorrowerRequestDto
 import com.api.v1.borrower.services.BorrowerSelfRegistrationService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
--- koltlin/v1/src/main/kotlin/com/api/v1/borrower/services/BorrowerSelfRegistrationService.kt
+++ koltlin/v1/src/main/kotlin/com/api/v1/borrower/services/BorrowerSelfRegistrationService.kt
@@ -1,7 +1,7 @@
 package com.api.v1.borrower.services
 
-import com.api.v1.borrower.helpers.BorrowerResponseDto
-import com.api.v1.borrower.helpers.NewBorrowerRequestDto
+import com.api.v1.borrower.dtos.BorrowerResponseDto
+import com.api.v1.borrower.dtos.NewBorrowerRequestDto
 import reactor.core.publisher.Mono
 
 interface BorrowerSelfRegistrationService {
