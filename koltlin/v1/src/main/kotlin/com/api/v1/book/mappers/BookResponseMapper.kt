fceeb907400294056cc2646d916a1f3f3608389b
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-09 10:13:29 -0300

    Created the mapper BookResponseMapper

---
+++ koltlin/v1/src/main/kotlin/com/api/v1/book/mappers/BookResponseMapper.kt
@@ -0,0 +1,24 @@
+package com.api.v1.book.mappers
+
+import com.api.v1.book.domain.Book
+import com.api.v1.book.dtos.BookResponseDto
+
+class BookResponseMapper {
+
+    companion object {
+        fun map(book: Book): BookResponseDto {
+            return BookResponseDto(
+                book.getFullTitle(),
+                book.isbn,
+                book.publisher,
+                book.version,
+                book.numberOfPages,
+                book.author,
+                book.field,
+                book.addedAt,
+                book.updatedAt
+            )
+        }
+    }
+
+}
