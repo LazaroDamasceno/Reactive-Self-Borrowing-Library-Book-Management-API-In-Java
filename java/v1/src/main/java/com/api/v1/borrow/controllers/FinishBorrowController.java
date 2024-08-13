ea5d79ce848435a236eb272042b8ade92b677da2
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-13 14:02:47 -0300

    Implemented the interface FinishBorrowService and created the controller FinishBorrowController

---
+++ java/v1/src/main/java/com/api/v1/borrow/controllers/FinishBorrowController.java
@@ -0,0 +1,25 @@
+package com.api.v1.borrow.controllers;
+
+import com.api.v1.annotations.ISBN;
+import com.api.v1.annotations.SSN;
+import com.api.v1.borrow.domain.Borrow;
+import com.api.v1.borrow.services.FinishBorrowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/borrows")
+public class FinishBorrowController {
+
+    @Autowired
+    private FinishBorrowService service;
+
+    @PatchMapping("finish/by-book/{isbn}/and/by-borrower{ssn}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public Mono<Borrow> finish(@ISBN @PathVariable String isbn, @SSN @PathVariable String ssn) {
+        return service.finish(isbn, ssn);
+    }
+
+}
--- java/v1/src/main/java/com/api/v1/borrow/services/FinishBorrowService.java
+++ java/v1/src/main/java/com/api/v1/borrow/services/FinishBorrowService.java
@@ -1,11 +1,10 @@
 package com.api.v1.borrow.services;
 
 import com.api.v1.borrow.domain.Borrow;
-import com.api.v1.borrower.domain.Borrower;
 import reactor.core.publisher.Mono;
 
 public interface FinishBorrowService {
 
-    Mono<Void> finish(Borrow borrow, Borrower borrower);
+    Mono<Borrow> finish(String isbn, String ssn);
 
 }
