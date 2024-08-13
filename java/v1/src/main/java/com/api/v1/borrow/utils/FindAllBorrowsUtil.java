6f938a3cabbb03334882bec717b50426615a3531
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-12 22:22:18 -0300

    Created the helper FindAllBorrowsUtil

---
+++ java/v1/src/main/java/com/api/v1/borrow/utils/FindAllBorrowsUtil.java
@@ -0,0 +1,63 @@
+package com.api.v1.borrow.utils;
+
+import com.api.v1.book.domain.Book;
+import com.api.v1.book.utils.BookFinderUtil;
+import com.api.v1.borrow.domain.Borrow;
+import com.api.v1.borrow.domain.BorrowRepository;
+import com.api.v1.borrower.domain.Borrower;
+import com.api.v1.borrower.utils.BorrowerFinderUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.time.ZonedDateTime;
+
+@Component
+public class FindAllBorrowsUtil {
+    
+    @Autowired
+    private BorrowRepository repository;
+
+    @Autowired
+    private BookFinderUtil bookFinder;
+
+    @Autowired
+    private BorrowerFinderUtil borrowerFinder;
+
+    public Flux<Borrow> findAllBorrows() {
+        return repository.findAll();
+    }
+
+    public Flux<Borrow> findAllActive() {
+        return repository
+                .findAll()
+                .filter(e -> e.getReturnedDate() == null);
+    }
+
+    public Flux<Borrow> findAllOverdue() {
+        return repository
+                .findAll()
+                .filter(e -> (
+                                (
+                                        ZonedDateTime.parse(e.getDueDate()).compareTo(getToday()) > 0
+                                        ||
+                                        ZonedDateTime.parse(e.getExtendedDueDate()).compareTo(getToday()) > 0
+                                ) && e.getReturnedDate() == null
+                ));
+    }
+
+    private ZonedDateTime getToday() {
+        int year = ZonedDateTime.now().getYear();
+        int month = ZonedDateTime.now().getMonthValue();
+        int day = ZonedDateTime.now().getDayOfMonth();
+        var zone = ZonedDateTime.now().getZone();
+        return ZonedDateTime.of(year, month, day, 0, 0, 0, 0, zone);
+    }
+
+    public Flux<Borrow> findAllFinished() {
+        return repository
+                .findAll()
+                .filter(e -> e.getReturnedDate() != null);
+    }
+    
+}
