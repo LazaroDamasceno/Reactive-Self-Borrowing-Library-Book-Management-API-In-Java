3c4b5115510e177490cf9ad04d30b41de700b9ef
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-14 23:42:35 -0300

    Deleted BorrowFluxMapper

--- v1/src/main/java/com/api/v1/borrow/controllers/FinishBorrowController.java
+++ v1/src/main/java/com/api/v1/borrow/controllers/FinishBorrowController.java
@@ -16,7 +16,7 @@ class FinishBorrowController {
     @Autowired
     private FinishBorrowService service;
 
-    @PatchMapping("finish/by-book/{isbn}/and/by-borrower{ssn}")
+    @PatchMapping("by-book/{isbn}/and/by-borrower{ssn}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public Mono<Borrow> finish(@ISBN @PathVariable String isbn, @SSN @PathVariable String ssn) {
         return service.finish(isbn, ssn);
