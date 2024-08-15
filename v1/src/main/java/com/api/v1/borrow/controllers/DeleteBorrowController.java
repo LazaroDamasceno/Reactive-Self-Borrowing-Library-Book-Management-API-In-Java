c20672e177e2c3e81a719b435c6c4a9dab051a26
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-15 14:46:00 -0300

    Created a service and a controller for deleting a borrow

---
+++ v1/src/main/java/com/api/v1/borrow/controllers/DeleteBorrowController.java
@@ -0,0 +1,26 @@
+package com.api.v1.borrow.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.api.v1.annotations.ISBN;
+import com.api.v1.annotations.SSN;
+import com.api.v1.borrow.services.DeleteBorrowService;
+
+import reactor.core.publisher.Mono;
+
+public class DeleteBorrowController {
+
+    @Autowired
+    private DeleteBorrowService service;
+
+    @DeleteMapping("by-borrower/{ssn}/and/by-book/{isbn}")    
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public Mono<Void> delete(@ISBN @PathVariable String isbn, @SSN @PathVariable String ssn) {
+        return service.delete(isbn, ssn);
+    }
+    
+}
