631a0de25cc4ef24a6c30be1cc9fcf7646920330
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2024-08-16 22:18:26 +0000

    Changed BorrowResponseMapper

--- v1/src/main/java/com/api/v1/borrow/mappers/BorrowResponseMapper.java
+++ v1/src/main/java/com/api/v1/borrow/mappers/BorrowResponseMapper.java
@@ -1,14 +1,16 @@
 package com.api.v1.borrow.mappers;
 
+import com.api.v1.book.mappers.BookDtoResponseMapper;
 import com.api.v1.borrow.domain.Borrow;
 import com.api.v1.borrow.dtos.BorrowResponseDto;
+import com.api.v1.borrower.mappers.BorrowerDtoResponseMapper;
 
 public class BorrowResponseMapper {
 
     public static BorrowResponseDto map(Borrow borrow) {
         return new BorrowResponseDto(
-                borrow.getBook(),
-                borrow.getBorrower(),
+                BookDtoResponseMapper.map(borrow.getBook()),
+                BorrowerDtoResponseMapper.map(borrow.getBorrower()),
                 borrow.getBorrowedDate(),
                 borrow.getDueDate(),
                 borrow.getExtendedDueDate(),
