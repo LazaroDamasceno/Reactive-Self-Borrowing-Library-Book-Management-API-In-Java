3fd8842669e4a1ce1f3669f0a715e66308f4c5e8
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-03 16:30:26 -0300

    Created the exception BorrowEntityNotExistException

--- v1/src/main/java/com/api/v1/borrow/exceptions/BorrowEntityNotExistException.java
+++ v1/src/main/java/com/api/v1/borrow/exceptions/BorrowEntityNotExistException.java
@@ -3,7 +3,7 @@ package com.api.v1.borrow.exceptions;
 public class BorrowEntityNotExistException extends RuntimeException {
 
     public BorrowEntityNotExistException() {
-        super("The entity Borrow no longer exists.");
+        super("The entity 'Borrow' no longer exists.");
     }
 
 }
