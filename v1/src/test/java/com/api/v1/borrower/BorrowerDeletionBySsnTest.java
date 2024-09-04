7b2b3874e2116f4c2c94e93bf51829d480632014
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-04 01:40:52 -0300

    Changed BorrowerDeletionBySsnTest

--- v1/src/test/java/com/api/v1/borrower/BorrowerDeletionBySsnTest.java
+++ v1/src/test/java/com/api/v1/borrower/BorrowerDeletionBySsnTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-
 public class BorrowerDeletionBySsnTest {
 
     @Autowired
