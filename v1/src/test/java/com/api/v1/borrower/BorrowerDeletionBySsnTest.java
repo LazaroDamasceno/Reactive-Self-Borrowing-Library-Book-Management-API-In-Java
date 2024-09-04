b8c34ba4f8a6768b15ca45752d310a8970821266
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-04 01:28:50 -0300

    Created BorrowerDeletionBySsnTest

---
+++ v1/src/test/java/com/api/v1/borrower/BorrowerDeletionBySsnTest.java
@@ -0,0 +1,35 @@
+package com.api.v1.borrower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class BorrowerDeletionBySsnTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testSuccessfulABorrowerBySSnDeletion() {
+        webTestClient
+                .delete()
+                .uri("api/v1/borrowers")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    void testUnsuccessfulAllBorrowerBySsnDeletion() {
+        webTestClient
+                .delete()
+                .uri("api/v1/borrowers")
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+}
