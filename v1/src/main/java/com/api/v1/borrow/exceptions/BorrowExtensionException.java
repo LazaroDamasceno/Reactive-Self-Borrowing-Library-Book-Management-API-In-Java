d617740b3c6c822b72e64590ecba544b18151c64
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-05 14:29:07 -0300

    Changed the message of BorrowExtensionException

--- v1/src/main/java/com/api/v1/borrow/exceptions/BorrowExtensionException.java
+++ v1/src/main/java/com/api/v1/borrow/exceptions/BorrowExtensionException.java
@@ -3,7 +3,7 @@ package com.api.v1.borrow.exceptions;
 public class BorrowExtensionException extends RuntimeException {
 
     public BorrowExtensionException() {
-        super("The borrow is already extended.");
+        super("The borrow cannot be extended.");
     }
 
 }
