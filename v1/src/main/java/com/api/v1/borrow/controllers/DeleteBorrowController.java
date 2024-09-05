887b987a0d59da9c874d866f69352d3de14f91fe
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-04 22:12:49 -0300

    Fixed DeleteBorrowController

--- v1/src/main/java/com/api/v1/borrow/controllers/DeleteBorrowController.java
+++ v1/src/main/java/com/api/v1/borrow/controllers/DeleteBorrowController.java
@@ -2,9 +2,7 @@ package com.api.v1.borrow.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.api.v1.annotations.ISBN;
 import com.api.v1.annotations.SSN;
@@ -12,12 +10,14 @@ import com.api.v1.borrow.services.DeleteBorrowService;
 
 import reactor.core.publisher.Mono;
 
+@RestController
+@RequestMapping("api/v1/borrows")
 public class DeleteBorrowController {
 
     @Autowired
     private DeleteBorrowService service;
 
-    @DeleteMapping("by-borrower/{ssn}/and/by-book/{isbn}")
+    @DeleteMapping("{isbn}/{ssn}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public Mono<Void> delete(@ISBN @PathVariable String isbn, @SSN @PathVariable String ssn) {
         return service.delete(isbn, ssn);
