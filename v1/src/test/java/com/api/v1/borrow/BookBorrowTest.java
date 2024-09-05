9551d124804d3ca68759dafa2116cd20cde90549
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-04 22:01:10 -0300

    Created the test class BookBorrowTest

---
+++ v1/src/test/java/com/api/v1/borrow/BookBorrowTest.java
@@ -0,0 +1,44 @@
+package com.api.v1.borrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BookBorrowTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void testSuccessfulBookBorrow() {
+
+        String isbn = "123456789012";
+        String ssn = "123456789";
+        String uri = String.format("api/v1/borrows/%s/%s", isbn, ssn);
+
+        webTestClient
+                .post()
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    public void testUnsuccessfulBookBorrow() {
+
+        String isbn = "123456789012";
+        String ssn = "123456789";
+        String uri = String.format("api/v1/borrows/%s/%s", isbn, ssn);
+
+        webTestClient
+                .post()
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+}
