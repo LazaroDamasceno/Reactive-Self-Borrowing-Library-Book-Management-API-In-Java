e4eeab367db32602aac0e8c79bf7aa57acf7b31c
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-05 00:11:23 -0300

    Changed the endpoints of ExtendBorrowController

--- v1/src/main/java/com/api/v1/borrow/controllers/ExtendBorrowController.java
+++ v1/src/main/java/com/api/v1/borrow/controllers/ExtendBorrowController.java
@@ -6,10 +6,7 @@ import com.api.v1.borrow.domain.Borrow;
 import com.api.v1.borrow.services.ExtendBorrowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -21,7 +18,7 @@ public class ExtendBorrowController {
 
     @PatchMapping("{isbn}/{ssn}/extension")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public Mono<Borrow> extend(@ISBN String isbn, @SSN String ssn) {
+    public Mono<Borrow> extend(@ISBN @PathVariable String isbn, @SSN @PathVariable String ssn) {
         return service.extend(isbn, ssn);
     }
 
