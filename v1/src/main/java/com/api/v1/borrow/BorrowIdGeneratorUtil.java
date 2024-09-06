501f3a63a207d9e4551fb62eefe15d656505dfbc
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-06 02:51:41 -0300

    Renamed BorrowIdGenerator as BorrowIdGeneratorUtil

--- v1/src/main/java/com/api/v1/borrow/BorrowIdGeneratorUtil.java
+++ v1/src/main/java/com/api/v1/borrow/BorrowIdGeneratorUtil.java
@@ -3,10 +3,10 @@ package com.api.v1.borrow;
 import java.math.BigInteger;
 import java.time.LocalDate;
 
-public class BorrowIdGenerator {
+public class BorrowIdGeneratorUtil {
 
-    private static final int currentYear = LocalDate.now().getYear();
-    private static final String batch = "%d000".formatted(currentYear);
+    private static int currentYear = LocalDate.now().getYear();
+    private static String batch = "%d000".formatted(currentYear);
     private static BigInteger id = new BigInteger(batch);
 
     public static BigInteger generateId() {
