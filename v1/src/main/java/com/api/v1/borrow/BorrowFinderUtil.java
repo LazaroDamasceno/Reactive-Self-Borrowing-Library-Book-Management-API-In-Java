6c726f3cec69a8a311189bfa55d447be1a87b7fe
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-06 05:09:51 -0300

    Fixed the filter of BorrowFinderUtil

--- v1/src/main/java/com/api/v1/borrow/BorrowFinderUtil.java
+++ v1/src/main/java/com/api/v1/borrow/BorrowFinderUtil.java
@@ -13,12 +13,13 @@ public class BorrowFinderUtil {
     private BorrowRepository borrowRepository;
 
     public Mono<Borrow> find(String id) {
-        BigInteger biId = new BigInteger(id);
         return borrowRepository
                 .findAll()
-                .filter(e -> e.getReturningDate() == null)
+                .filter(e -> e.getReturningDate() == null
+                    && e.getId().equals(new BigInteger(id))
+                )
                 .singleOrEmpty()
-                .switchIfEmpty(Mono.error(new BorrowNotFoundException(biId)));
+                .switchIfEmpty(Mono.error(new BorrowNotFoundException(new BigInteger(id))));
     }
 
 }
