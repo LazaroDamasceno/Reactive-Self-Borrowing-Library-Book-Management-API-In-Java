40a4c625360e22c65b5078f44c2cb4ff081acd81
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-06 19:46:10 -0300

    Deleted UnfinishableBorrowException

--- v1/src/main/java/com/api/v1/borrow/exceptions/UnfinishableBorrowException.java
+++
@@ -1,9 +0,0 @@
-package com.api.v1.borrow.exceptions;
-
-public class UnfinishableBorrowException extends RuntimeException {
-
-    public UnfinishableBorrowException(String id) {
-        super("borrow which id %s is unfinishable");
-    }
-
-}
