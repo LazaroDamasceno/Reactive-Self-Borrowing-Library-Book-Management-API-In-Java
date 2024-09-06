74d31b253c5cf8a2a464b0500613101c896d7ef0
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-06 02:08:57 -0300

    Changed BorrowResponseDto and BorrowResponseDto

--- v1/src/main/java/com/api/v1/borrow/BorrowMapper.java
+++ v1/src/main/java/com/api/v1/borrow/BorrowMapper.java
@@ -1,12 +1,15 @@
 package com.api.v1.borrow;
 
+import com.api.v1.book.mappers.BookResponseMapper;
+import com.api.v1.borrower.mappers.BorrowerResponseMapper;
+
 public class BorrowMapper {
 
     public static BorrowResponseDto map(Borrow borrow) {
         return new BorrowResponseDto(
                 borrow.getId(),
-                borrow.getBorrower(),
-                borrow.getBook(),
+                BorrowerResponseMapper.map(borrow.getBorrower()),
+                BookResponseMapper.map(borrow.getBook()),
                 borrow.getBorrowingDate(),
                 borrow.getDueDate()
         );
--- v1/src/main/java/com/api/v1/borrow/BorrowResponseDto.java
+++ v1/src/main/java/com/api/v1/borrow/BorrowResponseDto.java
@@ -1,14 +1,14 @@
 package com.api.v1.borrow;
 
-import com.api.v1.book.domain.Book;
-import com.api.v1.borrower.domain.Borrower;
+import com.api.v1.book.dtos.BookResponseDto;
+import com.api.v1.borrower.dtos.BorrowerResponseDto;
 
 import java.math.BigInteger;
 
 public record BorrowResponseDto(
         BigInteger id,
-        Borrower borrower,
-        Book book,
+        BorrowerResponseDto borrower,
+        BookResponseDto book,
         String borrowingDate,
         String dueDate
 ) {
